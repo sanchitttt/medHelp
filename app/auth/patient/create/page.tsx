cb3700d14ba613f68dacf6957a79d77885ad123f
Sanchit <sanchittewari222@gmail.com>
2023-07-03 19:40:30 +0530

    slight svg bug

--- app/auth/patient/create/page.tsx
+++ app/auth/patient/create/page.tsx
@@ -244,7 +244,7 @@ function PatientCreateForm() {
                     style={{ transform: 'translate(-50%,-50%)' }}
                 >
                     <div className='flex flex-col bg-white w-[250px] items-center h-[350px] justify-center gap-[30px] rounded-[10px]'>
-                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
+                        <svg className='tick-svg' version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.2 130.2">
                             <circle className="path circle" fill="none" stroke="#73AF55" strokeWidth="6" strokeMiterlimit="10" cx="65.1" cy="65.1" r="62.1" />
                             <polyline className="path check" fill="none" stroke="#73AF55" strokeWidth="6" strokeLinecap="round" strokeMiterlimit="10" points="100.2,40.2 51.5,88.8 29.8,67.5 " />
                         </svg>
